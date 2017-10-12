7064241578f85b344591874671cda0d2ed6c1906
tul1git <tula.patricio@gmail.com>
2017-10-12 10:03:02 +0200

    Removed innecessary files.

--- catkin_ws_turtleBotBasicControl/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tul1/Desktop/TurtleBotBasicControl/catkin_ws_turtleBotBasicControl/src
-# Build directory: /home/tul1/Desktop/TurtleBotBasicControl/catkin_ws_turtleBotBasicControl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(turtlebot_driver)
